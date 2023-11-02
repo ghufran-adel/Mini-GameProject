2fe29e5e2123b628d821a54eec13a434c728696c
ttrubiak <ttrubiak@gmail.com>
2023-11-02 00:38:47 -0500

    Blackjack theme, description, back button updated.

--- games-pages/Blackjack/Blackjack.js
+++ games-pages/Blackjack/Blackjack.js
@@ -36,6 +36,10 @@ const computerNumber = document.getElementById("computerNumber");
 const resultMessage = document.getElementById("resultMessage");
 const numberOptions = document.querySelectorAll(".numberOption");
 
+
+
+
+
 function getPick() {
     let goodPick = true;
     do {
@@ -98,4 +102,24 @@ goBackButton1.addEventListener('click', function() {
 const goBackButton2 = document.getElementById('back-arrow-back2');
 goBackButton2.addEventListener('click', function() {
   window.history.back();
+});
+
+
+// show or hide the game description
+document.addEventListener("DOMContentLoaded", function() 
+{
+    const descriptionButton = document.getElementById("descriptionButton");
+    const description = document.getElementById("description");
+
+    descriptionButton.addEventListener("click", function() 
+    {
+        if (description.style.display === "none" || description.style.display === "") 
+        {
+            description.style.display = "block";
+        } 
+        else 
+        {
+            description.style.display = "none"; 
+        }
+    });
 });
